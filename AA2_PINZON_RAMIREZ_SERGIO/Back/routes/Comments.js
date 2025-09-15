cf72721ce641f1c4f626b5306b0936c9eb1aa209
SPinzon01 <spinzonr@ucompensar.edu.co>
2025-09-14 21:51:29 -0500

    Delete AA2_PINZON_RAMIREZ_SERGIO directory

--- AA2_PINZON_RAMIREZ_SERGIO/Back/routes/Comments.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commentController = require("../controllers/CommentController");
-
-router.post("/", commentController.createComment);
-router.get("/:id", commentController.getCommentsByDeviceId);
-router.get("/", commentController.getAllComments);
-
-module.exports = router;
